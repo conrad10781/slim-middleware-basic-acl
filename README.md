318f702823ef01749aac0f4f38b533f53ed38e1a
Matthew C. Rice <mrice@rcs.us>
2018-01-07 12:31:45 -0500

    Initial commit for public usage

---
+++ README.md
@@ -0,0 +1,37 @@
+# PSR-7 HTTP Authentication ACL Middleware
+
+This middleware provides a very simple way to implement ACL in a PSR-7 environment.
+
+It has been tested  with [Slim Framework](http://www.slimframework.com/) using the [PSR-7 Basic Auth Middleware] provided by tuupola (https://github.com/tuupola/slim-basic-auth).
+
+## Install
+
+Install latest version using [composer](https://getcomposer.org/).
+
+```
+$ composer require rcs_us/slim-middleware-basic-acl
+```
+
+## Usage
+
+The middleware accepts two possible parameters. 
+
+1. An array of one or more usernames allowed to access the route. This compares against the value that is ultimately stored in $_SERVER["PHP_AUTH_USER"] | $request->getServerParams()["PHP_AUTH_USER"] .
+2. A callable that can do more complicated comparisons on $_SERVER["PHP_AUTH_USER"] | $request->getServerParams()["PHP_AUTH_USER"] .
+
+```php
+$app = new \Slim\App;
+// .... implement HTTP Authentication , IE $app->add(new \Slim\Middleware\HttpBasicAuthentication( ... 
+
+// array of usernames
+$app->get("/path/of/your/route", "\App\Controller\SomeController:methodToCall")->add(new \Slim\Middleware\HttpBasicACL(["username1", "username2"]));
+
+// callable
+$app->get("/path/of/your/route", "\App\Controller\SomeController:methodToCall")->add(new \Slim\Middleware\HttpBasicACL(function ($request, $response) {
+    // Some Logic Here Based On $_SERVER["PHP_AUTH_USER"] | $request->getServerParams()["PHP_AUTH_USER"] .
+    return false;
+}));
+
+```
+
+
---
+++ src/HttpBasicACL.php
@@ -0,0 +1,58 @@
+<?php 
+
+namespace Slim\Middleware;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class HttpBasicACL
+{
+    /**
+     * Allowed
+     *
+     * @var mixed
+     */
+    protected $_allowed = null;
+    
+    public function __construct($allowed) 
+    {
+        if ( !is_array($allowed) && !is_callable($allowed) ) {
+            throw new InvalidArgumentException('Allowed must be array or callable');
+        }
+        
+        $this->_allowed = $allowed;
+    }
+    
+    /**
+     * 
+     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
+     * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
+     * @param  callable                                 $next     Next middleware
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function __invoke(RequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        
+        if ( $this->_allowed != null ) {
+            if ( is_array($this->_allowed) ) {
+                if ( !in_array($request->getServerParams()["PHP_AUTH_USER"], $this->_allowed) ) {
+                    return $response->withStatus(401)
+                    ->withHeader("WWW-Authenticate", 'Basic realm="Protected"');
+                }
+            }
+            
+            if (is_callable($this->_allowed)) {
+                $method = $this->_allowed;
+                if (false === $method($request, $response)) {
+                    return $response->withStatus(401)
+                    ->withHeader("WWW-Authenticate", 'Basic realm="Protected"');
+                }
+            }
+        }
+        
+        $response = $next($request, $response);        
+        
+        return $response;
+    }
+}
